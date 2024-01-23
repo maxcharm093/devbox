maxcharm093/devbox

commit 2ce087f30e66d43bfc911e1293651fad3c08278a

[init-hooks] Prevent init hook recursion (#1709)
## Summary

Fixes https://github.com/jetpack-io/devbox/issues/1651

## How was it tested?

Added a `devbox run echo foobar` into our init hook. Tested with

```
devbox run echo 123
SHELL=fish devbox run echo 123
```

internal/devbox/devbox.go
@@ -338,7 +338,7 @@ func (d *Devbox) EnvVars(ctx context.Context) ([]string, error) {
 
 func (d *Devbox) shellEnvHashKey() string {
 	// Don't make this a const so we don't use it by itself accidentally
-	return "__DEVBOX_SHELLENV_HASH_" + d.projectDirHash()
+	return "__DEVBOX_SHELLENV_HASH_" + d.ProjectDirHash()
 }
 
 func (d *Devbox) Info(ctx context.Context, pkg string, markdown bool) (string, error) {
@@ -949,7 +949,7 @@ func (d *Devbox) computeEnv(ctx context.Context, usePrintDevEnvCache bool) (map[
 	devboxEnvPath = envpath.JoinPathLists(devboxEnvPath, runXPaths)
 
 	pathStack := envpath.Stack(env, originalEnv)
-	pathStack.Push(env, d.projectDirHash(), devboxEnvPath, d.preservePathStack)
+	pathStack.Push(env, d.ProjectDirHash(), devboxEnvPath, d.preservePathStack)
 	env["PATH"] = pathStack.Path(env)
 	debug.Log("New path stack is: %s", pathStack)
 
@@ -1170,7 +1170,7 @@ func (d *Devbox) setCommonHelperEnvVars(env map[string]string) {
 	env["LIBRARY_PATH"] = envpath.JoinPathLists(profileLibDir, env["LIBRARY_PATH"])
 }
 
-func (d *Devbox) projectDirHash() string {
+func (d *Devbox) ProjectDirHash() string {
 	h, _ := cachehash.Bytes([]byte(d.projectDir))
 	return h
 }

internal/shellgen/scripts.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"text/template"
+
+	_ "embed"
 
 	"github.com/pkg/errors"
 	"go.jetpack.io/devbox/internal/boxcli/featureflag"
@@ -14,6 +17,9 @@ import (
 	"go.jetpack.io/devbox/internal/lock"
 	"go.jetpack.io/devbox/internal/plugin"
 )
+
+//go:embed tmpl/init-hook.tmpl
+var initHookTmpl string
 
 const scriptsDir = ".devbox/gen/scripts"
 
@@ -27,6 +33,7 @@ type devboxer interface {
 	InstallablePackages() []*devpkg.Package
 	PluginManager() *plugin.Manager
 	ProjectDir() string
+	ProjectDirHash() string
 }
 
 // WriteScriptsToFiles writes scripts defined in devbox.json into files inside .devbox/gen/scripts.
@@ -55,7 +62,7 @@ func WriteScriptsToFiles(devbox devboxer) error {
 	}
 	hooks := strings.Join(append(pluginHooks, devbox.Config().InitHook().String()), "\n\n")
 	// always write it, even if there are no hooks, because scripts will source it.
-	err = writeHookFile(devbox, hooks)
+	err = writeInitHookFile(devbox, hooks)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -84,15 +91,25 @@ func WriteScriptsToFiles(devbox devboxer) error {
 	return nil
 }
 
-func writeHookFile(devbox devboxer, body string) (err error) {
+func writeInitHookFile(devbox devboxer, body string) (err error) {
 	script, err := createScriptFile(devbox, HooksFilename)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	defer script.Close() // best effort: close file
 
-	_, err = script.WriteString(body)
-	return errors.WithStack(err)
+	t, err := template.New("init-hook-template").Parse(initHookTmpl)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return t.Execute(script, map[string]any{
+		"Body":         body,
+		"InitHookHash": "__DEVBOX_INIT_HOOK_" + devbox.ProjectDirHash(),
+		// TODO put IsFish() in common place so we can call here and in devbox package
+		// without adding more stuff to interface
+		"IsFish": filepath.Base(os.Getenv("SHELL")) == "fish",
+	})
 }
 
 func WriteScriptFile(devbox devboxer, name, body string) (err error) {

internal/devbox/devbox_test.go
@@ -88,7 +88,7 @@ func TestComputeDevboxPathIsIdempotent(t *testing.T) {
 	t.Setenv("PATH", path)
 	t.Setenv(envpath.InitPathEnv, env[envpath.InitPathEnv])
 	t.Setenv(envpath.PathStackEnv, env[envpath.PathStackEnv])
-	t.Setenv(envpath.Key(devbox.projectDirHash()), env[envpath.Key(devbox.projectDirHash())])
+	t.Setenv(envpath.Key(devbox.ProjectDirHash()), env[envpath.Key(devbox.ProjectDirHash())])
 
 	env, err = devbox.computeEnv(ctx, false /*use cache*/)
 	require.NoError(t, err, "computeEnv should not fail")
@@ -110,7 +110,7 @@ func TestComputeDevboxPathWhenRemoving(t *testing.T) {
 	t.Setenv("PATH", path)
 	t.Setenv(envpath.InitPathEnv, env[envpath.InitPathEnv])
 	t.Setenv(envpath.PathStackEnv, env[envpath.PathStackEnv])
-	t.Setenv(envpath.Key(devbox.projectDirHash()), env[envpath.Key(devbox.projectDirHash())])
+	t.Setenv(envpath.Key(devbox.ProjectDirHash()), env[envpath.Key(devbox.ProjectDirHash())])
 
 	devbox.nix.(*testNix).path = ""
 	env, err = devbox.computeEnv(ctx, false /*use cache*/)

internal/devbox/envvars.go
@@ -76,5 +76,5 @@ func (d *Devbox) IsEnvEnabled() bool {
 	fakeEnv := map[string]string{}
 	// the Stack is initialized in the fakeEnv, from the state in the real os.Environ
 	pathStack := envpath.Stack(fakeEnv, envir.PairsToMap(os.Environ()))
-	return pathStack.Has(d.projectDirHash())
+	return pathStack.Has(d.ProjectDirHash())
 }

internal/devbox/refresh.go
@@ -15,7 +15,7 @@ func (d *Devbox) isRefreshAliasSet() bool {
 }
 
 func (d *Devbox) refreshAliasEnvVar() string {
-	return "DEVBOX_REFRESH_ALIAS_" + d.projectDirHash()
+	return "DEVBOX_REFRESH_ALIAS_" + d.ProjectDirHash()
 }
 
 func (d *Devbox) isGlobal() bool {

internal/shellgen/tmpl/init-hook.tmpl
@@ -1,0 +1,15 @@
+
+{{/* if hash is set, we're in recursive loop, just exit */ -}}
+if [ -n "${{ .InitHookHash }}" ]; then
+    return
+fi
+
+export {{ .InitHookHash }}=true
+
+{{ .Body }}
+
+{{ if .IsFish }}
+    set -e {{ .InitHookHash }}
+{{ else }}
+    unset {{ .InitHookHash }}
+{{ end }}

commit 2f2d7489821ad76f09903d4bcd1f5598c6311fbd

[fish] Fix init hooks for fish (#1741)
## Summary

Fixes issue introduced in https://github.com/jetpack-io/devbox/pull/1709

`devbox run` uses `sh` instead of native shell. So init hooks must
always be `sh` compatible for `run`. On the other hand, `devbox shell`
uses native shell so init hooks must be `fish` if the native shell is
fish.

This is ugly, but it's how it has always worked. 

https://github.com/jetpack-io/devbox/pull/1709 introduced recursion
protection that would be `fish` or `sh` depending on native shell. This
worked fine for `devbox shell` but would break `devbox run`.

This change fixes that by creating 2 hooks files, one fish and one sh
and sourcing the correct one in shellrc, while still always using the
`sh` for run.

cc: @Lagoja 

## How was it tested?

```
devbox run echo 5
devbox shell
SHELL=fish devbox run echo 5
SHELL=fish devbox shell
```

internal/devbox/devbox.go
@@ -204,7 +204,6 @@ func (d *Devbox) Shell(ctx context.Context) error {
 	}
 
 	opts := []ShellOption{
-		WithHooksFilePath(shellgen.ScriptPath(d.ProjectDir(), shellgen.HooksFilename)),
 		WithHistoryFile(filepath.Join(d.projectDir, shellHistoryFile)),
 		WithProjectDir(d.projectDir),
 		WithEnvVariables(envs),

internal/devbox/shell.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/alessio/shellescape"
 	"github.com/pkg/errors"
+	"go.jetpack.io/devbox/internal/shellgen"
 	"go.jetpack.io/devbox/internal/telemetry"
 
 	"go.jetpack.io/devbox/internal/debug"
@@ -59,8 +60,7 @@ type DevboxShell struct {
 	env             map[string]string
 	userShellrcPath string
 
-	hooksFilePath string
-	historyFile   string
+	historyFile string
 
 	// shellStartTime is the unix timestamp for when the command was invoked
 	shellStartTime time.Time
@@ -180,12 +180,6 @@ func initShellBinaryFields(path string) *DevboxShell {
 func WithHistoryFile(historyFile string) ShellOption {
 	return func(s *DevboxShell) {
 		s.historyFile = historyFile
-	}
-}
-
-func WithHooksFilePath(hooksFilePath string) ShellOption {
-	return func(s *DevboxShell) {
-		s.hooksFilePath = hooksFilePath
 	}
 }
 
@@ -317,8 +311,10 @@ func (s *DevboxShell) writeDevboxShellrc() (path string, err error) {
 	}()
 
 	tmpl := shellrcTmpl
+	hooksFilePath := shellgen.ScriptPath(s.projectDir, shellgen.HooksFilename)
 	if s.name == shFish {
 		tmpl = fishrcTmpl
+		hooksFilePath = shellgen.ScriptPath(s.projectDir, shellgen.HooksFishFilename)
 	}
 
 	err = tmpl.Execute(shellrcf, struct {
@@ -337,7 +333,7 @@ func (s *DevboxShell) writeDevboxShellrc() (path string, err error) {
 		ProjectDir:         s.projectDir,
 		OriginalInit:       string(bytes.TrimSpace(userShellrc)),
 		OriginalInitPath:   s.userShellrcPath,
-		HooksFilePath:      s.hooksFilePath,
+		HooksFilePath:      hooksFilePath,
 		ShellStartTime:     telemetry.FormatShellStart(s.shellStartTime),
 		HistoryFile:        strings.TrimSpace(s.historyFile),
 		ExportEnv:          exportify(s.env),

internal/shellgen/scripts.go
@@ -21,10 +21,17 @@ import (
 //go:embed tmpl/init-hook.tmpl
 var initHookTmpl string
 
+//go:embed tmpl/init-hook-fish.tmpl
+var initHookFishTmpl string
+
 const scriptsDir = ".devbox/gen/scripts"
 
 // HooksFilename is the name of the file that contains the project's init-hooks and plugin hooks
 const HooksFilename = ".hooks"
+
+// This is only used in shellrc_fish.tmpl. A bit of a hack, because scripts use
+// sh instead of fish.
+const HooksFishFilename = ".hooks.fish"
 
 type devboxer interface {
 	Config() *devconfig.Config
@@ -62,11 +69,16 @@ func WriteScriptsToFiles(devbox devboxer) error {
 	}
 	hooks := strings.Join(append(pluginHooks, devbox.Config().InitHook().String()), "\n\n")
 	// always write it, even if there are no hooks, because scripts will source it.
-	err = writeInitHookFile(devbox, hooks)
+	err = writeInitHookFile(devbox, hooks, initHookTmpl, HooksFilename)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	written[HooksFilename] = struct{}{}
+	err = writeInitHookFile(devbox, hooks, initHookFishTmpl, HooksFishFilename)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	written[HooksFishFilename] = struct{}{}
 
 	// Write scripts to files.
 	for name, body := range devbox.Config().Scripts() {
@@ -91,14 +103,14 @@ func WriteScriptsToFiles(devbox devboxer) error {
 	return nil
 }
 
-func writeInitHookFile(devbox devboxer, body string) (err error) {
-	script, err := createScriptFile(devbox, HooksFilename)
+func writeInitHookFile(devbox devboxer, body, tmpl, filename string) (err error) {
+	script, err := createScriptFile(devbox, filename)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	defer script.Close() // best effort: close file
 
-	t, err := template.New("init-hook-template").Parse(initHookTmpl)
+	t, err := template.New(filename).Parse(tmpl)
 	if err != nil {
 		return errors.WithStack(err)
 	}
@@ -106,10 +118,6 @@ func writeInitHookFile(devbox devboxer, body string) (err error) {
 	return t.Execute(script, map[string]any{
 		"Body":         body,
 		"InitHookHash": "__DEVBOX_INIT_HOOK_" + devbox.ProjectDirHash(),
-		// TODO put IsFish() in common place so we can call here and in devbox package
-		// without adding more stuff to interface
-		"IsFish": filepath.Base(os.Getenv("SHELL")) == "fish" ||
-			os.Getenv("FISH_VERSION") != "",
 	})
 }
 

internal/shellgen/tmpl/init-hook-fish.tmpl
@@ -1,12 +1,12 @@
-
+{{/* Fish version */ -}}
 {{/* if hash is set, we're in recursive loop, just exit */ -}}
 
-if [ -n "${{ .InitHookHash }}" ]; then
+if set -q {{ .InitHookHash }}; then
     return
-fi
+end
 
 export {{ .InitHookHash }}=true
 
 {{ .Body }}
 
-unset {{ .InitHookHash }}
+set -e {{ .InitHookHash }}

internal/devbox/shell_test.go
@@ -67,9 +67,8 @@ func testWriteDevboxShellrc(t *testing.T, testdirs []string) {
 			s := &DevboxShell{
 				devbox:          &Devbox{projectDir: projectDir},
 				env:             test.env,
-				projectDir:      "path/to/projectDir",
+				projectDir:      "/path/to/projectDir",
 				userShellrcPath: test.shellrcPath,
-				hooksFilePath:   test.hooksFilePath,
 			}
 			gotPath, err := s.writeDevboxShellrc()
 			if err != nil {

internal/devbox/testdata/shellrc/basic/shellrc.golden
@@ -15,7 +15,7 @@ export PS1="(devbox) $PS1"
 
 # Run plugin and user init hooks from the devbox.json directory.
 working_dir="$(pwd)"
-cd "path/to/projectDir" || exit
+cd "/path/to/projectDir" || exit
 
 # Source the hooks file, which contains the project's init hooks and plugin hooks.
 . /path/to/projectDir/.devbox/gen/scripts/.hooks.sh

internal/devbox/testdata/shellrc/noshellrc/shellrc.golden
@@ -9,7 +9,7 @@ export PS1="(devbox) $PS1"
 
 # Run plugin and user init hooks from the devbox.json directory.
 working_dir="$(pwd)"
-cd "path/to/projectDir" || exit
+cd "/path/to/projectDir" || exit
 
 # Source the hooks file, which contains the project's init hooks and plugin hooks.
 . /path/to/projectDir/.devbox/gen/scripts/.hooks.sh