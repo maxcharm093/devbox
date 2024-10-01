maxcharm093/devbox

commit dd9d373090472ab95499e5bcf4ba334f2016247a

[sandboxes] devbox ls: print versions (#2277)
## Summary

For packages without an explicit version, this PR will print the version
for
the user in `devbox ls`.

Fixes #2274 

## How was it tested?
```
% devbox ls
* go@latest - 1.23.0
* runx:golangci/golangci-lint@latest - 1.60.2
* runx:mvdan/gofumpt@latest - 0.7.0
* cowsay@3.8.2
```
Notice that versions are printed for go@latest and the runx packages,
but
not for cowsay since it was added via `devbox add cowsay@3.8.2`

Also, `devbox ls -c examples/flakes/php` and `devbox ls -c
examples/flakes/remote`
```
% devbox ls -c examples/flakes/remote
* github:nixos/nixpkgs/5233fd2ba76a3accb5aaa999c00509a11fd0793c#hello
* github:nixos/nixpkgs/5233fd2ba76a3accb5aaa999c00509a11fd0793c#cowsay
* github:F1bonacc1/process-compose/v0.43.1
```

internal/devbox/devbox.go
@@ -842,10 +842,15 @@ func (d *Devbox) flakeDir() string {
 // flakes  (lockfile vs devbox list)
 func (d *Devbox) AllPackageNamesIncludingRemovedTriggerPackages() []string {
 	result := []string{}
-	for _, p := range d.cfg.Packages(true /*includeRemovedTriggerPackages*/) {
-		result = append(result, p.VersionedName())
+	for _, p := range d.AllPackagesIncludingRemovedTriggerPackages() {
+		result = append(result, p.Versioned())
 	}
 	return result
+}
+
+func (d *Devbox) AllPackagesIncludingRemovedTriggerPackages() []*devpkg.Package {
+	packages := d.cfg.Packages(true /*includeRemovedTriggerPackages*/)
+	return devpkg.PackagesFromConfig(packages, d.lockfile)
 }
 
 // AllPackages returns the packages that are defined in devbox.json and

internal/boxcli/list.go
@@ -5,6 +5,7 @@ package boxcli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -32,8 +33,26 @@ func listCmd() *cobra.Command {
 			if err != nil {
 				return errors.WithStack(err)
 			}
-			for _, p := range box.AllPackageNamesIncludingRemovedTriggerPackages() {
-				fmt.Fprintf(cmd.OutOrStdout(), "* %s\n", p)
+
+			for _, pkg := range box.AllPackagesIncludingRemovedTriggerPackages() {
+				resolvedVersion, err := pkg.ResolvedVersion()
+				if err != nil {
+					// Continue to print the package even if we can't resolve the version
+					// so that the user can see the error for this package, as well as get the
+					// results for the other packages
+					resolvedVersion = "<error resolving version>"
+				}
+				msg := ""
+
+				// Print the resolved version, unless the user has specified a version already
+				if strings.HasSuffix(pkg.Versioned(), "latest") && resolvedVersion != "" {
+					// Runx packages have a "v" prefix (why?). Trim for consistency.
+					resolvedVersion = strings.TrimPrefix(resolvedVersion, "v")
+					msg = fmt.Sprintf("* %s - %s\n", pkg.Versioned(), resolvedVersion)
+				} else {
+					msg = fmt.Sprintf("* %s\n", pkg.Versioned())
+				}
+				fmt.Fprint(cmd.OutOrStdout(), msg)
 			}
 			return nil
 		},

internal/devpkg/package.go
@@ -832,3 +832,15 @@ func packageInstallErrorHandler(err error, pkg *Package, installableOrEmpty stri
 
 	return usererr.WithUserMessage(err, "error installing package %s", pkg.Raw)
 }
+
+func (p *Package) ResolvedVersion() (string, error) {
+	if err := p.resolve(); err != nil {
+		return "", err
+	}
+	lockPackage := p.lockfile.Get(p.Raw)
+	// Flake packages don't have any values in the lockfile
+	if lockPackage == nil {
+		return "", nil
+	}
+	return p.lockfile.Get(p.Raw).Version, nil
+}

commit 0f302d9a8840e7ca5031c7cc191c8354f2eea51a

[lockfile] Fix tidy for non-versioned packages (#2313)
## Summary

Fixes issue introduced in https://github.com/jetify-com/devbox/pull/2277

This comment explains it pretty well:
https://github.com/jetify-com/devbox/pull/2277/files#r1767085866

except:

> I think this doesn't matter in practice, because we always append
@latest during devbox add <name>.

This is not 100% true. In the very small number of packages that are not
in nixhub but are valid attribute paths, (e.g. `stdenv.cc.cc.lib`) we
store the key in the lockfile without `@latest` :(. The key missmatch
caused lockfile.Tidy to remove entries.

My bad for making the suggestion that lead to this, this was really
subtle difference.

## How was it tested?

Added `stdenv.cc.cc.lib` on devbox.json linux only, installed on linux
and copied over lockfile to macos. Did `devbox install` and ensured
lockfile was not modified.

internal/devbox/devbox.go
@@ -853,8 +853,8 @@ func (d *Devbox) flakeDir() string {
 // flakes  (lockfile vs devbox list)
 func (d *Devbox) AllPackageNamesIncludingRemovedTriggerPackages() []string {
 	result := []string{}
-	for _, p := range d.AllPackagesIncludingRemovedTriggerPackages() {
-		result = append(result, p.Versioned())
+	for _, p := range d.cfg.Packages(true /*includeRemovedTriggerPackages*/) {
+		result = append(result, p.VersionedName())
 	}
 	return result
 }