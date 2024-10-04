maxcharm093/devbox

commit 6a72354a9c9daaeb957bfa98c2c2202173f4762e

[telemetry] Don't log user errors (#2305)
## Summary

We're running out of Sentry events and it looks like the culprit is
devbox overlogging, including a lot of user errors that should not be
logged.

This PR https://github.com/jetify-com/devbox/pull/844 removed code that
avoided logging user errors. I think this was a bug? If programmer wants
a user error to be logged, they should use `WithLoggedUserMessage`
instead.

## How was it tested?

Untested

internal/boxcli/midcobra/telemetry.go
@@ -43,7 +43,7 @@ func (m *telemetryMiddleware) postRun(cmd *cobra.Command, args []string, runErr 
 	defer telemetry.Stop()
 
 	var userExecErr *usererr.ExitError
-	if errors.As(runErr, &userExecErr) {
+	if errors.As(runErr, &userExecErr) || !usererr.ShouldLogError(runErr) {
 		return
 	}
 

commit 7462e1ee3aaf9369a47dcba495ed2e8b924e0a9a

[logging] Fix segment logging (#2322)
## Summary

Fixes bug introduced in https://github.com/jetify-com/devbox/pull/2305

## How was it tested?

internal/boxcli/midcobra/telemetry.go
@@ -8,11 +8,9 @@ import (
 	"runtime/trace"
 	"sort"
 
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"go.jetpack.io/devbox/internal/boxcli/featureflag"
-	"go.jetpack.io/devbox/internal/boxcli/usererr"
 	"go.jetpack.io/devbox/internal/devbox"
 	"go.jetpack.io/devbox/internal/devbox/devopt"
 	"go.jetpack.io/devbox/internal/envir"
@@ -42,11 +40,6 @@ func (m *telemetryMiddleware) postRun(cmd *cobra.Command, args []string, runErr 
 	defer trace.StartRegion(cmd.Context(), "telemetryPostRun").End()
 	defer telemetry.Stop()
 
-	var userExecErr *usererr.ExitError
-	if errors.As(runErr, &userExecErr) || !usererr.ShouldLogError(runErr) {
-		return
-	}
-
 	meta := telemetry.Metadata{
 		FeatureFlags: featureflag.All(),
 		CloudRegion:  os.Getenv(envir.DevboxRegion),
@@ -68,6 +61,7 @@ func (m *telemetryMiddleware) postRun(cmd *cobra.Command, args []string, runErr 
 
 	if runErr != nil {
 		telemetry.Error(runErr, meta)
+		// TODO: This is skipping event logging of calls that end in error. We probably want to log them.
 		return
 	}
 	telemetry.Event(telemetry.EventCommandSuccess, meta)

internal/boxcli/usererr/usererr.go
@@ -83,9 +83,14 @@ func Extract(err error) (error, bool) { // nolint: revive
 	return nil, false
 }
 
-// ShouldLogError returns true if the it's a logged user error or is a non-user error
+// ShouldLogError returns true if the it's a combined error specifically marked to be logged
+// or if it's not an ExitError.
 func ShouldLogError(err error) bool {
 	if err == nil {
+		return false
+	}
+	var userExecErr *ExitError
+	if errors.As(err, &userExecErr) {
 		return false
 	}
 	c := &combined{}

internal/telemetry/telemetry.go
@@ -24,6 +24,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	segment "github.com/segmentio/analytics-go"
+	"go.jetpack.io/devbox/internal/boxcli/usererr"
 	"go.jetpack.io/devbox/internal/devbox/providers/identity"
 	"go.jetpack.io/devbox/internal/nix"
 
@@ -138,7 +139,8 @@ func commandEvent(meta Metadata) (id string, msg *segment.Track) {
 // Error reports an error to the telemetry server.
 func Error(err error, meta Metadata) {
 	errToLog := err // use errToLog to avoid shadowing err later. Use err to keep API clean.
-	if !started || errToLog == nil {
+
+	if !started || !usererr.ShouldLogError(errToLog) {
 		return
 	}
 